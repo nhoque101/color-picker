50a2d93ac3671c1de47ea6b7bf988e185d08d241
nhoque101 <naimul991@gmail.com>
2024-12-26 12:48:41 -0500

    ColorPicker.jsx created and initialized

---
+++ src/ColorPicker.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+
+function ColorPicker(){
+
+    const [color, setColor]= useState("");
+
+
+    return(
+        <>
+        
+        
+        </>
+    )
+}
+
+export default ColorPicker
